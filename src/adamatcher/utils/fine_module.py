4836ce7b070c0003bbc26a3f2275099a7bf55034
PaBut <xbuten00@vutbr.cz>
2025-03-18 22:06:58 +0100

    .

--- src/adamatcher/utils/fine_module.py
+++ src/adamatcher/utils/fine_module.py
@@ -285,8 +285,10 @@ class FineModule(nn.Module):
                         feat_d8_1[bs_b_ids1_l1, bs_j_ids1_l1],
                     ]
                     if data["zs"].sum() > 0:
-                        feat_d8.append(feat_f0_z.flatten(1, 2))
-                        feat_d8.append(feat_f1_z.flatten(1, 2))
+                        # feat_d8.append(feat_f0_z.flatten(1, 2))
+                        # feat_d8.append(feat_f1_z.flatten(1, 2))
+                        feat_d8.append(feat_f0_z[zs_b_ids[indices], zs_ci_ids[indices]])
+                        feat_d8.append(feat_f1_z[zs_b_ids[indices], zs_ci_ids[indices]])
 
                     logger.info(f"feat_d8 shapes: {[feat.shape for feat in feat_d8]}, , {torch.cat([bs_kptsfeat0_from1, bs_kptsfeat1],0).shape}")
 
@@ -368,8 +370,10 @@ class FineModule(nn.Module):
                         feat_d8_1[bs_b_ids0_l1, bs_i_ids0_l1],
                     ]
                     if data["zs"].sum() > 0:
-                        feat_d8.append(feat_f0_z.flatten(1, 2))
-                        feat_d8.append(feat_f1_z.flatten(1, 2))
+                        # feat_d8.append(feat_f0_z.flatten(1, 2))
+                        # feat_d8.append(feat_f1_z.flatten(1, 2))
+                        feat_d8.append(feat_f0_z[zs_b_ids[indices], zs_ci_ids[indices]])
+                        feat_d8.append(feat_f1_z[zs_b_ids[indices], zs_ci_ids[indices]])
 
                     logger.info(f"feat_d8 shapes: {[feat.shape for feat in feat_d8]}, {torch.cat([bs_kptsfeat0, bs_kptsfeat1_from0],0).shape}")
 
