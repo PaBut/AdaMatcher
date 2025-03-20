bb35930af699e12f7fff98237292c91686e40d96
PaBut <xbuten00@vutbr.cz>
2025-03-20 17:03:10 +0100

    .

--- src/adamatcher/utils/fine_module.py
+++ src/adamatcher/utils/fine_module.py
@@ -299,11 +299,12 @@ class FineModule(nn.Module):
                     #     feat_d8.append(feat_f1_z[zs_b_ids[indices], zs_ci_ids[indices]])
 
                     logger.info(f"feat_d8 shapes: {[feat.shape for feat in feat_d8]}, , {torch.cat([bs_kptsfeat0_from1, bs_kptsfeat1],0).shape}")
-                    logger.info(f"bs_feat_c: {bs_feat_c.shape}")
 
                     bs_feat_c = self.down_proj(
                         torch.cat(feat_d8, dim=0)
                     )  # [2n, 2c->c]
+
+                    logger.info(f"bs_feat_c: {bs_feat_c.shape}")
                     
                     bs_feat_cf = self.merge_feat(
                         torch.cat(
