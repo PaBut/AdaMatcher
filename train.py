75955ea12179d81d5b047e454723d500d8ecbd6e
PaBut <xbuten00@vutbr.cz>
2025-03-23 16:48:18 +0100

    .

--- train.py
+++ train.py
@@ -214,7 +214,7 @@ def main():
     trainer = pl.Trainer(
         strategy=DDPStrategy(
             find_unused_parameters=False,  # True,
-            num_nodes=args.num_nodes,
+            # num_nodes=args.num_nodes,
             # strategy="ddp_sharded",
             sync_batchnorm=config.TRAINER.WORLD_SIZE > 0,
         ),
