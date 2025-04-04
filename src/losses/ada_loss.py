a4dccef978f2cdc8cfb85f797274fdbaad0d3f23
PaBut <xbuten00@vutbr.cz>
2025-04-05 01:54:56 +0200

    .

--- src/losses/ada_loss.py
+++ src/losses/ada_loss.py
@@ -358,7 +358,7 @@ class AdaMatcherLoss(nn.Module):
             else:
                 gt_pt0_l2 = data["zs_pt0_f_float"][b_ids0_l1]
             pt0 = data['kpts0_l2']  # * s0_l2
-            # logger.info(f"gt_pt0_l2: {gt_pt0_l2.shape}, pt0: {pt0.shape}")
+            logger.info(f"gt_pt0_l2: {gt_pt0_l2.shape}, pt0: {pt0.shape}")
             p_mask0 = (pt0 == gt_pt0_l2).all(-1)
             pt0, gt_pt0_l2, b_ids0_l1, i_ids0_l1, j_ids0_l1 = (
                 pt0[p_mask0],
