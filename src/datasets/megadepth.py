d3463bbbc3a71554e1e85366e7186797fe86e3b9
PaBut <xbuten00@vutbr.cz>
2025-04-06 13:13:42 +0200

    fixed

--- src/datasets/megadepth.py
+++ src/datasets/megadepth.py
@@ -74,9 +74,7 @@ class MegaDepthDataset(Dataset):
         self.is_walkdepth = kwargs.get('walk_depth', False)
 
         if self.is_walkdepth:
-            self.root_dir = osp.join(self.root_dir, self.scene_id)
-            logger.info(
-                f'Using walkdepth dataset: {self.root_dir} for {self.scene_id}')
+            self.root_dir = self.scene_id
 
     def __len__(self):
         return len(self.pair_infos)
