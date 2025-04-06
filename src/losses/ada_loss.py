46c2e41549d72c0fdae25f62be08ebcf586ffabb
PaBut <xbuten00@vutbr.cz>
2025-04-06 17:50:26 +0200

    .

--- src/losses/ada_loss.py
+++ src/losses/ada_loss.py
@@ -345,7 +345,8 @@ class AdaMatcherLoss(nn.Module):
                 data['spv_w_pt1_i_l1'] / s0_l2.unsqueeze(1),
                 (data['spv_pt1_i_l1'] / s1_l2.unsqueeze(1)).round(),
             )
-            logger.info(f"{spv_w_pt0_i_l2}, {spv_pt0_i_l2}")
+            logger.info(f"{spv_w_pt0_i_l2[(spv_w_pt0_i_l2[..., 0] > 0) & (spv_w_pt0_i_l2[..., 1] > 0)]}")
+            logger.info(f"{spv_pt0_i_l2[(spv_pt0_i_l2[..., 0] > 0) & (spv_pt0_i_l2[..., 1] > 0)]}")
 
         b_ids0_l1, i_ids0_l1, j_ids0_l1 = (
             data['b_ids0_l2'],
