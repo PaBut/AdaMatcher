d7eaac8ac1d0235ee530df97ec92983fd6cd1713
PaBut <xbuten00@vutbr.cz>
2025-04-07 23:56:09 +0200

    fixed potential issue

--- src/losses/ada_loss.py
+++ src/losses/ada_loss.py
@@ -149,7 +149,7 @@ class AdaMatcherLoss(nn.Module):
                 logger.warning(
                     'assign a false supervision to avoid ddp deadlock')
                 # pdb.set_trace()
-            loss_valid = torch.tensor(0.0)
+            loss_valid = torch.tensor(float('inf'))
         else:
             loss_valid = (((r_w_pt1[correct_mask] - gt_r_w_pt1[correct_mask])**
                            2).sum(-1) * weight[correct_mask]).mean()
