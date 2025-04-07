126136c3b5bed98d18890bc6c9699fe590bf14da
PaBut <xbuten00@vutbr.cz>
2025-04-07 10:26:34 +0200

    set epoch count as 10

--- scripts/reproduce_train/32gpus_1bs_832_WalkDepth.sh
+++ scripts/reproduce_train/32gpus_1bs_832_WalkDepth.sh
@@ -32,4 +32,4 @@ python3 -u ./train.py \
     --num_sanity_val_steps=10 \
     --benchmark=True \
     --ckpt_path=${ckpt_path} \
-    --max_epochs=5 >> ./OUTPUT/AdaMatcher.txt
+    --max_epochs=10 >> ./OUTPUT/AdaMatcher.txt
