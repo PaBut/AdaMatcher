b39e8f210d592b8be6fc7d4df847dae541ef33ae
PaBut <xbuten00@vutbr.cz>
2025-04-07 23:56:41 +0200

    chanhed dataset dir

--- configs/data/walkdepth_trainval_832.py
+++ configs/data/walkdepth_trainval_832.py
@@ -12,7 +12,7 @@ _CN.TRAINER = CN()
 
 _CN.NJOBS = 1  # x scenes
 
-TRAINVAL_BASE_PATH = "data/WalkDepthDb"
+TRAINVAL_BASE_PATH = "data/WalkDepth"
 
 _CN.DATASET.TRAIN_DATA_SOURCE = "MegaDepth"
 _CN.DATASET.TRAIN_WALKDEPTH = True
