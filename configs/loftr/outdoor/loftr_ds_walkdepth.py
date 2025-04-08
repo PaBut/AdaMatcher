a6cc53107bba1a7f8f7fd27807dd04b896f491f5
PaBut <xbuten00@vutbr.cz>
2025-04-08 10:47:34 +0200

    set lower lr

--- configs/loftr/outdoor/loftr_ds_walkdepth.py
+++ configs/loftr/outdoor/loftr_ds_walkdepth.py
@@ -2,7 +2,7 @@ from src.config.default import _CN as cfg
 
 cfg.ADAMATCHER.MATCH_COARSE.MATCH_TYPE = "dual_softmax"
 
-cfg.TRAINER.CANONICAL_LR = 5e-5
+cfg.TRAINER.CANONICAL_LR = 5e-6
 cfg.TRAINER.WARMUP_STEP = 500
 cfg.TRAINER.WARMUP_RATIO = 0.1
 cfg.TRAINER.MSLR_GAMMA = 0.4
