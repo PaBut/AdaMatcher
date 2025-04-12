0c3ed7064238e2ad77a191ce94e74e33651f0650
PaBut <xbuten00@vutbr.cz>
2025-04-12 02:43:37 +0200

    another try

--- src/datasets/megadepth.py
+++ src/datasets/megadepth.py
@@ -195,7 +195,9 @@ class MegaDepthDataset(Dataset):
                     # ], dtype=np.float32)
                     
                     K_0[0, 2] = scale_wh0[0] * scale0[0] - K_0[0, 2]
-                    K_1[0, 2] = scale_wh0[0] * scale0[0] - K_1[0, 2]
+                    K_0[0, 0] = -K_0[0, 0]
+                    K_1[0, 2] = scale_wh0[0] * scale1[0] - K_1[0, 2]
+                    K_1[0, 0] = -K_1[0, 0]
 
                     flip = patrial(KT.hflip)
                 else: 
@@ -205,8 +207,10 @@ class MegaDepthDataset(Dataset):
                     #     [0,  0, 1]
                     # ], dtype=np.float32)
                     
-                    K_0[1, 2] = scale_wh0[1] * scale0[1] - 1 - K_0[1, 2]
-                    K_1[1, 2] = scale_wh0[1] * scale0[1] - 1 - K_1[1, 2]
+                    K_0[1, 2] = scale_wh0[1] * scale0[1] - K_0[1, 2]
+                    K_0[1, 1] = -K_0[1, 1]
+                    K_1[1, 2] = scale_wh0[1] * scale1[1] - K_1[1, 2]
+                    K_1[1, 1] = -K_1[1, 1]
 
                     flip = patrial(KT.vflip)
 
