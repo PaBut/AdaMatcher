f8567b1ef9a61ed146363c90a830b67ff3738341
PaBut <xbuten00@vutbr.cz>
2025-04-12 02:02:21 +0200

    ...

--- src/datasets/megadepth.py
+++ src/datasets/megadepth.py
@@ -32,7 +32,7 @@ def apply_rotation_matrix_pose(pose, matrix):
     R = pose[:3, :3]
     t = pose[:3, 3]
 
-    R_new = R @ matrix  # Rotate camera
+    R_new = R @ matrix.cpu().numpy()  # Rotate camera
     pose_new = torch.eye(4)
     pose_new[:3, :3] = R_new
     pose_new[:3, 3] = t
